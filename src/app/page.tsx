c234ee75855f23a97d571a48179eab4187c43465
hito-kotaro <tohi.work@gmail.com>
2024-01-07 21:29:27 +0900

    既存ファイルを整理

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-	return (
-		<h1>hello</h1>
-	)
+  return <h1 className="bg-red-200">hello</h1>;
 }
