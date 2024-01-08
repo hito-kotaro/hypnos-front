deeac053ede0c3ebbe1e0d119b429ff21c974f5a
hito-kotaro <tohi.work@gmail.com>
2024-01-08 11:17:18 +0900

    投稿一覧整列

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,33 +19,79 @@ const Page = () => {
     { title: "test", name: "kotaro", date: "2024/01/01" },
   ];
   return (
-    <Box>
-      <Box>最近の投稿</Box>
+    <Box className="px-2 my-2">
+      <Box>
+        <Typography variant="h4" className="text-text">
+          最近の投稿
+        </Typography>
+      </Box>
       {/*投稿一覧*/}
       {/*記事リストアイテム*/}
-      <Box className="bg-red-300 flex-wrap justify-center">
-        {data.map((u: User, i: number) => {
-          if (i === 0) {
+
+      <Box className="mt-20">
+        <Grid container spacing={2}>
+          {data.map((u: User, i: number) => {
             return (
-              <PostMiniIcon
+              <Grid
                 key={i}
-                title={u.title}
-                name={u.name}
-                date={u.date}
-              />
+                item
+                xs={12}
+                sx={{ display: { xs: "block", md: "none" } }}
+              >
+                <Box className="flex justify-center">
+                  <PostMiniIcon name={u.name} title={u.title} date={u.date} />
+                </Box>
+              </Grid>
             );
-          } else {
-            return (
-              <PostMiniIcon
-                key={i}
-                title={u.title}
-                name={u.name}
-                date={u.date}
-              />
-            );
-          }
-        })}
+          })}
+        </Grid>
+
+        <Grid container spacing={2} className="bg-sky-200">
+          {data.map((u: User, i: number) => {
+            if (i % 2 === 0) {
+              return (
+                <Grid
+                  key={i}
+                  item
+                  md={6}
+                  sx={{ display: { xs: "none", md: "block" } }}
+                  className="bg-red-200"
+                >
+                  <Box className="flex justify-end">
+                    <PostMiniIcon name={u.name} title={u.title} date={u.date} />
+                  </Box>
+                </Grid>
+              );
+            } else {
+              return (
+                <Grid
+                  key={i}
+                  item
+                  md={6}
+                  sx={{ display: { xs: "none", md: "block" } }}
+                  className="bg-red-200"
+                >
+                  <Box className="flex justify-start">
+                    <PostMiniIcon name={u.name} title={u.title} date={u.date} />
+                  </Box>
+                </Grid>
+              );
+            }
+          })}
+        </Grid>
       </Box>
+
+      {/*
+      <Grid container>
+        {data.map((u: User, i: number) => {
+          return (
+            <Grid key={i} item xs={6} className="bg-red-200">
+              <PostMiniIcon name={u.name} title={u.title} date={u.date} />
+            </Grid>
+          );
+        })}
+      </Grid>
+			*/}
     </Box>
   );
 };
